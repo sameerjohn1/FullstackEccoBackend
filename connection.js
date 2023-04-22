a6223a39de709a41f4f09dd86b2a885654369c04
sameerjohn1 <sameerprogrammer5@gmail.com>
2023-04-22 11:21:41 +0500

    first commit

---
+++ connection.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const connectionStr =
+  "mongodb+srv://CompleteMernEco:CompleteMernEco@cluster0.zws51l6.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionStr, { useNewUrlparser: true })
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
